ac43e787b392366d0404e1b8609f253b1a26298e
79276 <rushan.zur@yandex.ru>
2023-12-19 22:02:39 +0500

    13

--- conftest.py
+++ conftest.py
@@ -32,19 +32,22 @@ def config(request, scope='session'):
     # Return config so it can be used
     return config
 
+
 @pytest.fixture
 def browser(config):
+
     # Initialize the WebDriver instance
     if config['browser'] == 'Chrome':
         opts = webdriver.ChromeOptions()
         if config['headless']:
             opts.add_argument('headless')
-        b = webdriver.Chrome(ChromeDriverManager().install())
+        b = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
     elif config['browser'] == 'Firefox':
         opts = webdriver.FirefoxOptions()
         if config['headless']:
             opts.headless = True
-        b = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        b = webdriver.Firefox(
+            executable_path=GeckoDriverManager().install(), options=opts)
     else:
         raise Exception(f'Browser "{config["browser"]}" is not supported')
 
@@ -57,12 +60,11 @@ def browser(config):
     # Quit the WebDriver instance for the teardown
     b.quit()
 
-# Quit the WebDriver instance for the teardown
-    b.quit()
 
 @pytest.fixture
 def datatable():
     return DataTable()
+
 
 class DataTable(object):
 
@@ -78,10 +80,12 @@ class DataTable(object):
     def __repr__(self) -> str:
         return self.__str__()
 
+
 @given(parsers.parse('I have navigated to the \'the-internet\' "{page_name}" page'), target_fixture='navigate_to')
 def navigate_to(browser, page_name):
     url = BasePage.PAGE_URLS.get(page_name.lower())
     browser.get(url)
+
 
 @then(parsers.parse('a "{text}" banner is displayed in the top-right corner of the page'))
 def verify_banner_text(browser, text):
@@ -99,9 +103,11 @@ def verify_banner_text(browser, text):
         if attr.startswith("border"):
             assert "0px" == attr.split(": ")[1]
 
+
 @then(parsers.parse('the page has a footer containing "{text}"'))
 def verify_footer_text(browser, text):
     assert text == BasePage(browser).get_page_footer_text()
+
 
 @then(parsers.parse('the link in the page footer goes to "{url}"'))
 def verify_footer_link_url(browser, url):
